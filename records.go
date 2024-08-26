7a87c29dbaac4611c6e634aeea0e0be11b2fc7db
hizuheka <44028436+hizuheka@users.noreply.github.com>
2024-08-27 08:13:07 +0900

    .

--- records.go
+++ records.go
@@ -7,8 +7,18 @@ import (
 
 type Records []Record
 
-func (rs Records) Sort() {
-	sort.SliceStable(a, func(i, j int) bool {
-		return a[i].CreatedAt.Before(a[j].CreatedAt)
-	})
+func (r Records) Sort() {
+        sort.Slice(r, func(i, j int) bool {
+                if r[i].region.Start.MachiCode < r[j].region.Start.MachiCode {
+                        return true
+                } else if r[i].region.Start.BanCode < r[j].region.Start.BanCode {
+                        return true
+                } else if r[i].region.Start.EdaCode < r[j].region.Start.EdaCode {
+                        return true
+                } else if r[i].region.Start.KoedaCode < r[j].region.Start.KoedaCode {
+                        return true
+                } else {
+                        return false
+                }
+        })
 }
